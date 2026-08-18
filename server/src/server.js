3c4851965537a41821cbe1b97f0704bfa15a5bb3
oyejeet <oyeejeet@gmail.com>
2026-08-18 05:34:16 +0530

    Fixed the api endpoint for server services

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
